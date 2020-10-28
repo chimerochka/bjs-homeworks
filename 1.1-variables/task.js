1f3d048df3ff56f75e08f39402e545970bf54778
Anna Pan <pann.anna@mail.ru>
2020-10-29 00:50:13 +0300

    first commit varibles

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -5,23 +5,35 @@
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
 
-function averageMark(a, g, p) {
+function averageMark() {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = 5;
+	let geography = 3;
+	let physics = 4;
+	let sum = 3;
+	
+	return (algebra + geography + physics) / sum;
 }
+averageMark();
 
-function sayHello(userName) {
+
+function sayHello() {
 	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = 'userName'; 	
+    let message = `Привет, мир! Меня зовут ${myName}`;
+	
+	return message;
 }
+sayHello();
+
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	
+	let x = 2;
+    let y = 22;
+    let z = 0;
+	let result = x * y + 5 * z + x - 1;
+
+	return result;
 }
+calculateFormula();
